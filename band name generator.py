632ab98cce9123042021c6a2c846acb0e37cc4af
Shreeganth <shreeganth.cse@gmail.com>
2024-10-20 12:34:05 +0530

    first release

---
+++ band name generator.py
@@ -0,0 +1,4 @@
+print("This will generate a band name for your band")
+city_name = input("\nEnter the name of your city : ")
+pet_name = input("\nEnter the name of your pet : ")
+band_name = input(f"\n The Band name will be {city_name+" "+pet_name}")
