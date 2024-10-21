01daf0e7741729ba652fc19f840e22a09907e8d8
Shreeganth <shreeganth.cse@gmail.com>
2024-10-21 20:39:24 +0530

    this is the first version which is pure error but nothing at all ill work it up i will be commiting this to see what i have done in future

--- Password Generator.py
+++ Password Generator.py
@@ -6,25 +6,31 @@ pass_numbers = int(input("How many numbers do you like in your password? "))
 alphabets = "abcdefghiklmnopqrstuvwxyz"
 symbols = "!@#$%^&*"
 numbers = "1234567890"
-easy_password = ""
+easy_password = []
 for i in range(pass_letters):
     easy_password.append(random.choice(alphabets))
     easy_password.append(random.choice(symbols))
     easy_password.append(random.choice(numbers))
 
-easy_password = str(easy_password)
-print("the Easy Password is: ", easy_password)
+easy_password1 = ""
+for i in easy_password:
+    easy_password1 += i
+
+print("the Easy Password is: ", easy_password1)
 
 alphabets = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-uppercase_alphabets = [letter.upper() for letter in alphabets]
-alphabets = alphabets + uppercase_alphabets
 
 tough_password = []
-mixture = [uppercase_alphabets,symbols,numbers]
+mixture = [alphabets,symbols,numbers]
 
 for i in range(pass_letters):
     tough_password.append(random.choice(mixture))
 
-print("the Tough Password is: ", tough_password)
+easy_password1 = ""
+
+for i in tough_password:
+    easy_password1 += i
+
+print("the Tough Password is: ", easy_password1)
 
 
