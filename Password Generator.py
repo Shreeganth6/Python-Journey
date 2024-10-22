f7f4f5f9dab542c5a2b38c4bb64412bfe2634d36
Shreeganth <shreeganth.cse@gmail.com>
2024-10-22 22:23:35 +0530

    deletion of password generator

--- Password Generator.py
+++ Password Generator.py
@@ -1,28 +1,39 @@
 import random
+import string
+
 print("Welcome to PyPassword Generator")
+
 pass_letters = int(input("How many letters do you like in your password? "))
 pass_symbols = int(input("How many symbols do you like in your password? "))
 pass_numbers = int(input("How many numbers do you like in your password? "))
-alphabets = "abcdefghiklmnopqrstuvwxyz"
-symbols = "!@#$%^&*"
-numbers = "1234567890"
-easy_password = ""
-for i in range(pass_letters):
-    easy_password.append(random.choice(alphabets))
-    easy_password.append(random.choice(symbols))
-    easy_password.append(random.choice(numbers))
 
-easy_password = str(easy_password)
-print("the Easy Password is: ", easy_password)
+la = list(string.ascii_lowercase)
+ua = list(string.ascii_uppercase)
 
-alphabets = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-uppercase_alphabets = [letter.upper() for letter in alphabets]
-alphabets = alphabets + uppercase_alphabets
 
-tough_password = []
-mixture = [uppercase_alphabets,symbols,numbers]
+alphabets = la + ua
 
-for i in range(pass_letters):
-    tough_password.append(random.choice(mixture))
+number = ['1','2','3','4','5','6','7','8','9','0']
 
-print("the Tough Password is: ", tough_password)
+symbols = ['!','@','#','$','%','^','&','*','(',')','_',]
+
+#password
+
+password =[]
+
+for character in range(0,pass_letters):
+    password.append(random.choice(alphabets))
+for character in range(0,pass_numbers):    
+    password.append(random.choice(number))
+for character in range(0,pass_symbols):
+    password.append(random.choice(symbols))
+
+random.shuffle(password)
+password = password[:pass_letters]
+print(password)
+str_password = ""
+
+for letter in range(0,len(password)):
+    str_password = str_password + password[letter]
+
+print(f"The password is {str_password}")
